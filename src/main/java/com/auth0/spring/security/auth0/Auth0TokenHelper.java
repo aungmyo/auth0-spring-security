2143a2e5e3b1df53767546ac639b722bfe88a51f
Richard Seldon <arcseldon@gmail.com>
2016-06-02 03:47:41 +0900

    cleanup

--- src/main/java/com/auth0/spring/security/auth0/Auth0TokenHelper.java
+++
@@ -1,9 +0,0 @@
-package com.auth0.spring.security.auth0;
-
-public interface Auth0TokenHelper<T> {
-
-    public String generateToken(T object, long expiration);
-
-    public T decodeToken(String token);
-
-}
